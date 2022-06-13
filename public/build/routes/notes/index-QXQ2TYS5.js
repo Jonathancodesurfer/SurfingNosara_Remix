ae1facea80b52846697b372e1dd3e780f7057200
Jonathancodesurfer <jonathan@ivcmexico.com>
2022-06-13 14:50:50 -0600

    Commit

---
+++ public/build/routes/notes/index-QXQ2TYS5.js
@@ -0,0 +1,23 @@
+import {
+  Link
+} from "/build/_shared/chunk-OPWEH4M3.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:/Users/jonathanalpizar/Documents/Dev/SurfingNosara_Remix/app/routes/notes/index.jsx?browser
+init_react();
+
+// app/routes/notes/index.jsx
+init_react();
+function NoteIndexPage() {
+  return /* @__PURE__ */ React.createElement("p", null, "No note selected. Select a note on the left, or", " ", /* @__PURE__ */ React.createElement(Link, {
+    to: "new",
+    className: "text-blue-500 underline"
+  }, "create a new note."));
+}
+export {
+  NoteIndexPage as default
+};
+//# sourceMappingURL=/build/routes/notes/index-QXQ2TYS5.js.map
