baac9c5753f0bb6b8d04805a8c772b035118d24c
Jonathancodesurfer <jonathan@ivcmexico.com>
2022-06-13 19:16:39 -0600

    first commit

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,3 @@
-import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,33 +7,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
-
-export const links = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
-
 export const meta = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const loader = async ({ request }) => {
-  return json({
-    user: await getUser(request),
-  });
-};
-
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
